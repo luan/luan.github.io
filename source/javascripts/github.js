a568af2977c10619cad277d39c996fbe214b0a91
Luan Santos <cfcluan@gmail.com>
2012-07-13 22:03:27 -0300

    APIs

--- source/javascripts/github.js
+++ source/javascripts/github.js
@@ -1,34 +1,46 @@
-var github = (function(){
-  function render(target, repos){
-    var i = 0, fragment = '', t = $(target)[0];
+var github = (function() {
+  function render(target, repos) {
+    var i = 0,
+      t = $(target)[0];
+    var fragment = '';
 
-    for(i = 0; i < repos.length; i++) {
-      fragment += '<li><a href="'+repos[i].url+'">'+repos[i].name+'</a><p>'+repos[i].description+'</p></li>';
+    for (i = 0; i < repos.length; i++) {
+      fragment += '<li><a href="' + repos[i].html_url + '">' + repos[i].name + '</a> <em>' + repos[i].description + '</em></li>';
     }
     t.innerHTML = fragment;
   }
   return {
-    showRepos: function(options){
+    showRepos: function(options) {
       $.ajax({
-          url: "http://github.com/api/v2/json/repos/show/"+options.user+"?callback=?"
-        , type: 'jsonp'
-        , error: function (err) { $(options.target + ' li.loading').addClass('error').text("Error loading feed"); }
-        , success: function(data) {
+        url: "https://api.github.com/users/" + options.user + "/repos?callback=?",
+        type: 'jsonp',
+        error: function(err) {
+          $(options.target + ' li.loading').addClass('error').text("Error loading feed");
+        },
+        success: function(data) {
           var repos = [];
-          if (!data || !data.repositories) { return; }
-          for (var i = 0; i < data.repositories.length; i++) {
-            if (options.skip_forks && data.repositories[i].fork) { continue; }
-            repos.push(data.repositories[i]);
+          if (!data || !data.data) {
+            return;
+          }
+          for (var i = 0; i < data.data.length; i++) {
+            if (options.skip_forks && data.data[i].fork) {
+              continue;
+            }
+            repos.push(data.data[i]);
           }
           repos.sort(function(a, b) {
             var aDate = new Date(a.pushed_at).valueOf(),
-                bDate = new Date(b.pushed_at).valueOf();
+              bDate = new Date(b.pushed_at).valueOf();
 
-            if (aDate === bDate) { return 0; }
+            if (aDate === bDate) {
+              return 0;
+            }
             return aDate > bDate ? -1 : 1;
           });
 
-          if (options.count) { repos.splice(options.count); }
+          if (options.count) {
+            repos.splice(options.count);
+          }
           render(options.target, repos);
         }
       });
